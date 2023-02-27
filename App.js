49c1fd5c2996ca1d64925825bb74353724c6cadb
Andras E <rohadtsajt@gmail.com>
2023-02-27 12:45:14 +0000

    merge plus npm stack

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SplashScreen from './src/screens/splash';
+import SignInScreen from './src/screens/signin';
+
+
+const stack = createNativeStackNavigator();
+
+const App = () => {
+  return <>
+
+    <NavigationContainer>
+      <StatusBar
+        backgroundColor="transparent"
+        translucent={true}
+      />
+
+      <stack.Navigator>
+        <stack.Screen name={"splash"} component={SplashScreen} options={{ headerShown: false }} />
+        <stack.Screen name={"signIn"} component={SignInScreen} options={{ headerShown: false }} />
+      </stack.Navigator>
+    </NavigationContainer>
+  </>
+};
+
+export default App;
