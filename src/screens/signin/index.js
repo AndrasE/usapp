46c9e27b7d982dc9e6fddf719a0724082b4f791a
Andras E <rohadtsajt@gmail.com>
2023-02-27 12:34:37 +0000

    merge src and font config

---
+++ src/screens/signin/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import {ImageBackground, Text, TouchableWithoutFeedback} from 'react-native';
+import Lottie from 'lottie-react-native';
+import {_signInWithGoogle} from '../../config/firebase/GoogleSingIn.js';
+
+export default function SignInScreen({}) {
+  async function googleSignin() {
+    _signInWithGoogle().then(data => {
+      if (!data) {
+        console.log('=> Error', 'no data');
+        return;
+      }
+      console.log('=> Success', data);
+    });
+  }
+
+  return (
+    <ImageBackground
+      source={require('../../assets/signin.jpg')}
+      resizeMode={'cover'}
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 30,
+      }}>
+      <Text
+        style={{
+          fontSize: 35,
+          fontFamily: 'SpaceMonoRegular',
+          color: 'white',
+          paddingBottom: 30,
+          textAlign: 'center',
+        }}>
+        sign-in with your Google account
+      </Text>
+      <TouchableWithoutFeedback onPress={() => googleSignin()}>
+        <Lottie
+          style={{width: 200}}
+          source={require('../../assets/google-singin.json')}
+          autoPlay
+          loop={false}
+          speed={1.2}
+        />
+      </TouchableWithoutFeedback>
+    </ImageBackground>
+  );
+}
---
+++ src/screens/splash/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Text, ImageBackground } from 'react-native';
+import Animated, { FadeIn } from 'react-native-reanimated';
+import Lottie from 'lottie-react-native';
+
+export default function SplashScreen({ navigation }) {
+
+    setTimeout(() => {
+      this.anim.play()
+    }, 1000);
+    setTimeout(() => {
+      navigation.navigate("signIn")
+    }, 2100);
+
+  return (
+    <ImageBackground
+      source={require('../../assets/signin.jpg')}
+      resizeMode={'cover'}
+      style={{ flex: 1, justifyContent: "center", alignItems: "center", padding: 30 }}>
+      <Animated.View
+        entering={FadeIn.duration(500).delay(100)}
+      >
+        <Text style={{ fontSize: 35, fontFamily: "SpaceMonoRegular", color: 'white', letterSpacing: 5 }}>Welcome</Text>
+      </Animated.View>
+      <Animated.View
+        entering={FadeIn.duration(500).delay(250)}
+      >
+        <Text style={{ fontFamily: "SpaceMonoRegular", fontSize: 25, marginTop: 68, color: "black", padding: 55 }}>to</Text>
+      </Animated.View>
+      <Lottie style={{ paddingTop: 70 }} source={require('../../assets/splash.json')} autoPlay={false} loop={false} speed={0.7} ref={animation => { this.anim = animation }}
+      />
+      <Animated.View
+        entering={FadeIn.duration(600).delay(800)}
+      >
+        <Text style={{ letterSpacing: 2.5, fontFamily: "SpaceMonoRegular", fontSize: 75, paddingTop: 25, color: "#fff" }}>us</Text>
+      </Animated.View>
+    </ImageBackground>
+  );
+}
