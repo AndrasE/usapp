e1e73ee32523d9b26a216cb3f44fef3a8efbf5da
Andras E <rohadtsajt@gmail.com>
2023-03-01 13:24:56 +0000

    t

--- App.js
+++ App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import SplashScreen from './src/screens/splash.js';
-import SignInScreen from './src/screens/signin.js';
-import ChatScreen from './src/screens/chat.js';
+import SplashScreen from './src/screens/splash';
+import SignInScreen from './src/screens/signin';
+import ChatScreen from './src/screens/chat';
 
 const stack = createNativeStackNavigator();
 
 const App = () => {
   return <>
-
     <NavigationContainer>
       <stack.Navigator>
         <stack.Screen name={"splash"} component={SplashScreen} options={{ headerShown: false }} />
