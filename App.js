d803d7e6aa99de4864f8543ccfc0b9afae03f6a9
Andras E <rohadtsajt@gmail.com>
2023-03-03 15:15:05 +0000

    Update App.js

--- App.js
+++ App.js
@@ -1,24 +1,55 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React, {useState, useEffect} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import auth from '@react-native-firebase/auth';
+
 import SplashScreen from './src/screens/splash';
 import SignInScreen from './src/screens/signin';
 import HomeScreen from './src/screens/home';
 
 const Stack = createNativeStackNavigator();
 
-
 const App = () => {
-  return <>
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name={"splash"} component={SplashScreen} options={{ headerShown: false }} />
-        <Stack.Screen name={"signIn"} component={SignInScreen} options={{ headerShown: false }} />
-        <Stack.Screen name={"home"} component={HomeScreen} options={{ headerShown: false }} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  </>
-};
+  // Set an initializing state whilst Firebase connects
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
 
+  // Handle user state changes
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }  
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  if (initializing) return null;
+
+  return (
+    <>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name={'splash'}
+            component={SplashScreen}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name={'signIn'}
+            component={SignInScreen}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name={'home'}
+            component={HomeScreen}
+            options={{headerShown: false}}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+};
 
 export default App;
