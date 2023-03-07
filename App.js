5246fc48770f8e0444a99b6a98772e4c8164bd08
Andras E <rohadtsajt@gmail.com>
2023-03-07 21:29:33 +0000

    ni

--- App.js
+++ App.js
@@ -1,41 +1,17 @@
-import React, {useState, createContext ,useEffect} from 'react';
+import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import auth from '@react-native-firebase/auth';
-import { UserAuthContextProvider } from './src/config/context/userAuthContext';
+import {UserAuthContextProvider} from './src/config/context/userAuthContext';
 import SplashScreen from './src/screens/splash';
 import SignInScreen from './src/screens/signin';
 import HomeScreen from './src/screens/home';
-import { useUserAuth } from './src/config/context/userAuthContext';
 
 const Stack = createNativeStackNavigator();
 
-
 const App = () => {
-  // // Set an initializing state whilst Firebase connects
-  // const [initializing, setInitializing] = useState(true);
-  // const [user, setUser] = useState();
-
-
-  // // Handle user state changes
-  // function onAuthStateChanged(user) {
-  //   setUser(user);
-  //   if (initializing) setInitializing(false);
-  //   console.log(user);
-  // }  
-
-  // useEffect(() => {
-  //   const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
-  //   return subscriber; // unsubscribe on unmount
-  // }, []);
-
-  // if (initializing) return null;
-
   return (
-    <>
     <UserAuthContextProvider>
       <NavigationContainer>
-        
         <Stack.Navigator>
           <Stack.Screen
             name={'splash'}
@@ -54,8 +30,7 @@ const App = () => {
           />
         </Stack.Navigator>
       </NavigationContainer>
-      </UserAuthContextProvider>
-    </>
+    </UserAuthContextProvider>
   );
 };
 
