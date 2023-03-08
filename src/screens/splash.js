b532104a3dd17cbf46c2df33ad73f826cc7b66e6
Andras E <rohadtsajt@gmail.com>
2023-03-08 21:41:22 +0000

    splash + root
    splash timout until firebase initialize,
    and rooting working, conditionally navigated to correct screen, w an error msg:
    
    If you're trying to navigate to a screen in a nested navigator, see https://reactnavigation.org/docs/nesting-navigators#navigating-to-a-screen-in-a-nested-navigator

--- src/screens/splash.js
+++ src/screens/splash.js
@@ -1,38 +1,71 @@
 import React from 'react';
-import { Text, ImageBackground } from 'react-native';
-import Animated, { FadeIn } from 'react-native-reanimated';
+import {Text, ImageBackground} from 'react-native';
+import Animated, {FadeIn} from 'react-native-reanimated';
 import Lottie from 'lottie-react-native';
 
-export default function SplashScreen({ navigation }) {
+export default function SplashScreen() {
+  setTimeout(() => {
+    this.anim.play();
+  }, 1000);
 
-    setTimeout(() => {
-      this.anim.play()
-    }, 1000);
-    setTimeout(() => {
-      navigation.navigate("signIn")
-    }, 2100);
+  // setTimeout(() => {
+  //   navigation.replace("signIn")
+  // }, 2100);
 
   return (
     <ImageBackground
       source={require('../assets/signin.jpg')}
       resizeMode={'cover'}
-      style={{ flex: 1, justifyContent: "center", alignItems: "center", padding: 30 }}>
-      <Animated.View
-        entering={FadeIn.duration(500).delay(100)}
-      >
-        <Text style={{ fontSize: 35, fontFamily: "SpaceMonoRegular", color: 'white', letterSpacing: 5 }}>Welcome</Text>
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 30,
+      }}>
+      <Animated.View entering={FadeIn.duration(500).delay(100)}>
+        <Text
+          style={{
+            fontSize: 35,
+            fontFamily: 'SpaceMonoRegular',
+            color: 'white',
+            letterSpacing: 5,
+          }}>
+          Welcome
+        </Text>
       </Animated.View>
-      <Animated.View
-        entering={FadeIn.duration(500).delay(250)}
-      >
-        <Text style={{ fontFamily: "SpaceMonoRegular", fontSize: 25, marginTop: 68, color: "black", padding: 55 }}>to</Text>
+      <Animated.View entering={FadeIn.duration(500).delay(250)}>
+        <Text
+          style={{
+            fontFamily: 'SpaceMonoRegular',
+            fontSize: 25,
+            marginTop: 68,
+            color: 'black',
+            padding: 55,
+          }}>
+          to
+        </Text>
       </Animated.View>
-      <Lottie style={{ paddingTop: 70 }} source={require('../assets/splash.json')} autoPlay={false} loop={false} speed={0.7} ref={animation => { this.anim = animation }}
+      <Lottie
+        style={{paddingTop: 70}}
+        source={require('../assets/splash.json')}
+        autoPlay={false}
+        loop={false}
+        speed={0.7}
+        ref={animation => {
+          this.anim = animation;
+        }}
       />
-      <Animated.View
-        entering={FadeIn.duration(600).delay(800)}
-      >
-        <Text style={{ letterSpacing: 2.5, fontFamily: "SpaceMonoRegular", fontSize: 75, paddingTop: 25, color: "#fff" }}>us</Text>
+      <Animated.View entering={FadeIn.duration(600).delay(800)}>
+        <Text
+          style={{
+            letterSpacing: 2.5,
+            fontFamily: 'SpaceMonoRegular',
+            fontSize: 75,
+            paddingTop: 25,
+            color: '#fff',
+          }}>
+          us
+        </Text>
       </Animated.View>
     </ImageBackground>
   );
