54e938461583e5e5cb32378a174d13e2ab56e90a
Andras E <rohadtsajt@gmail.com>
2023-03-14 21:51:23 +0000

    privacy pop up screen
    added pop up screen and linear gradient package

--- src/navigations/CustomDrawer.js
+++ src/navigations/CustomDrawer.js
@@ -6,12 +6,9 @@ import {
 } from '@react-navigation/drawer';
 import {useUserAuth} from '../config/context/userAuthContext';
 
-
-
 const CustomDrawer = props => {
-    const {user} = useUserAuth();
-    const profileImgUrl = user.photoURL;
-
+  const {user} = useUserAuth();
+  const profileImgUrl = user.photoURL;
 
   return (
     <View style={{flex: 1}}>
@@ -22,15 +19,27 @@ const CustomDrawer = props => {
           source={require('../assets/drawercover.jpg')}
           style={{height: 170, width: '100%'}}>
           <Image
-            source={{uri:profileImgUrl}}
+            source={{uri: profileImgUrl}}
             style={{
               height: 80,
               width: 80,
               borderRadius: 40,
-              margin: 20,
+              margin: 10,
               alignSelf: 'flex-end',
             }}
           />
+          <Text style={{ fontSize: 30,
+          fontFamily: 'SpaceMonoRegular',
+          color: 'white',
+          letterSpacing: 5,
+          marginRight: 10,
+          textAlign: "right"}}> {user.displayName.split(' ')[0]}</Text>
+          <Text style={{ fontSize: 15,
+          fontFamily: 'SpaceMonoRegular',
+          color: 'white',
+          letterSpacing: 3,
+          marginRight: 10,
+          textAlign: "right"}}> {user.email}</Text>
         </ImageBackground>
         <DrawerItemList {...props} />
       </DrawerContentScrollView>
