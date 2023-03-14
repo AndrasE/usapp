40cdba9278f21b537b86eb9c078ab6a301a044f7
Andras E <rohadtsajt@gmail.com>
2023-03-14 13:29:48 +0000

    finishing drawer nav
    plus adding custimDrawer.js

---
+++ src/navigations/ScreensImport.js
@@ -0,0 +1,18 @@
+import SplashScreen from '../screens/splash';
+import SignInScreen from '../screens/signin';
+import HomeScreen from '../screens/home';
+import AsdScreen from '../screens/asd';
+import Asd2Screen from '../screens/asd2';
+import Asd3Screen from '../screens/asd3';
+
+// just to have everything in one place, so i wont need
+// to import all screens line by line
+
+export {
+  SplashScreen,
+  SignInScreen,
+  HomeScreen,
+  AsdScreen,
+  Asd2Screen,
+  Asd3Screen,
+};
