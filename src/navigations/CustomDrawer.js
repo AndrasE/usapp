5002c0ca4a1c7606e237a83bb5dc52ccaf09d88b
Andras E <rohadtsajt@gmail.com>
2023-03-15 22:22:30 +0000

    vector icons added + some styling

--- src/navigations/CustomDrawer.js
+++ src/navigations/CustomDrawer.js
@@ -17,7 +17,7 @@ const CustomDrawer = props => {
         contentContainerStyle={{paddingTop: 0}}>
         <ImageBackground
           source={require('../assets/drawercover.jpg')}
-          style={{height: 170, width: '100%', marginBottom:15}}>
+          style={{height: 170, width: '100%', marginBottom: 15}}>
           <Image
             source={{uri: profileImgUrl}}
             style={{
@@ -26,22 +26,34 @@ const CustomDrawer = props => {
               borderRadius: 40,
               margin: 10,
               alignSelf: 'flex-end',
-              borderColor: "white",
-              borderWidth: 1
+              borderColor: 'white',
+              borderWidth: 1,
             }}
           />
-          <Text style={{ fontSize: 30,
-          fontFamily: 'SpaceMonoRegular',
-          color: 'white',
-          letterSpacing: 5,
-          marginRight: 10,
-          textAlign: "right"}}> {user.displayName.split(' ')[0]}</Text>
-          <Text style={{ fontSize: 15,
-          fontFamily: 'SpaceMonoRegular',
-          color: 'white',
-          letterSpacing: 3,
-          marginRight: 10,
-          textAlign: "right"}}> {user.email}</Text>
+          <Text
+            style={{
+              fontSize: 30,
+              fontFamily: 'SpaceMonoRegular',
+              color: 'white',
+              letterSpacing: 5,
+              marginRight: 10,
+              textAlign: 'right',
+            }}>
+            {' '}
+            {user.displayName.split(' ')[0]}
+          </Text>
+          <Text
+            style={{
+              fontSize: 15,
+              fontFamily: 'SpaceMonoRegular',
+              color: 'white',
+              letterSpacing: 3,
+              marginRight: 10,
+              textAlign: 'right',
+            }}>
+            {' '}
+            {user.email}
+          </Text>
         </ImageBackground>
         <DrawerItemList {...props} />
       </DrawerContentScrollView>
