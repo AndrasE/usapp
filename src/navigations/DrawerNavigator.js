3869594e0d0b4381b7e29b01e1fbb9be5fe63afb
Andras E <rohadtsajt@gmail.com>
2023-03-15 22:22:30 +0000

    vector icons added + some styling

--- src/navigations/DrawerNavigator.js
+++ src/navigations/DrawerNavigator.js
@@ -5,7 +5,6 @@ import {HomeScreen, AsdScreen, Asd2Screen, Asd3Screen} from './ScreensImport';
 
 import Icon from 'react-native-vector-icons/Ionicons';
 
-
 const Drawer = createDrawerNavigator();
 
 function DrawerNavigator() {
@@ -14,29 +13,75 @@ function DrawerNavigator() {
       drawerContent={props => <CustomDrawer {...props} />}
       screenOptions={{
         headerShown: false,
-        drawerActiveBackgroundColor: "#3780a3",
-        drawerActiveTintColor: "white",
+        drawerActiveBackgroundColor: '#3780a3',
+        drawerActiveTintColor: 'white',
         drawerLabelStyle: {
-          marginLeft: 30,
           fontFamily: 'SpaceMonoRegular',
           letterSpacing: 2,
-          fontSize: 17
-        }
+          fontSize: 17,
+        },
       }}
       swipeEdgeWidth={500}>
-      <Drawer.Screen name="Chats" component={HomeScreen} 
-       options={{
-        title: 'Home',
-        drawerIcon: ({focused, color, size}) => (
-          <Icon name="hipchat" size={18} color={color} />
-        ),
-      }}
-      
-      
+      <Drawer.Screen
+        name="Chats"
+        component={HomeScreen}
+        options={{
+          title: 'Home',
+          drawerIcon: ({color, size}) => (
+            <Icon
+              name="md-chatbubble-ellipses-outline"
+              size={25}
+              color={color}
+              style={{marginLeft: 50}}
+            />
+          ),
+        }}
       />
-      <Drawer.Screen name="Penis" component={AsdScreen} />
-      <Drawer.Screen name="Pussy" component={Asd2Screen} />
-      <Drawer.Screen name="Cheese" component={Asd3Screen} />
+      <Drawer.Screen
+        name="Search"
+        component={AsdScreen}
+        options={{
+          title: 'Search',
+          drawerIcon: ({color, size}) => (
+            <Icon
+              name="search-sharp"
+              size={25}
+              color={color}
+              style={{marginLeft: 50}}
+            />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="ChatGpt"
+        component={Asd2Screen}
+        options={{
+          title: 'ChatGpt',
+          drawerIcon: ({color, size}) => (
+            <Icon
+              name="md-git-compare"
+              size={25}
+              color={color}
+              style={{marginLeft: 50}}
+            />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Theme"
+        component={Asd2Screen}
+        options={{
+          title: 'Theme',
+          drawerIcon: ({color, size}) => (
+            <Icon
+              name="md-partly-sunny-outline"
+              size={25}
+              color={color}
+              style={{marginLeft: 50}}
+            />
+          ),
+        }}
+      />
     </Drawer.Navigator>
   );
 }
