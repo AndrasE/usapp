784afafd6f488e20cecfc780766f430e2d6c0766
Andras E <rohadtsajt@gmail.com>
2023-03-19 17:07:33 +0000

    firebase db test 1
    creating user

--- src/config/context/userAuthContext.js
+++ src/config/context/userAuthContext.js
@@ -2,18 +2,33 @@ import React, {useState, createContext, useContext, useEffect} from 'react';
 import {GoogleSignin} from '@react-native-google-signin/google-signin';
 import auth from '@react-native-firebase/auth';
 import initalizeFirebaseDb from '../firebase/Firebase';
-
-//imported here  from Firebase.js so when authentication happen firebase is initalized as well, otherwise will be error:
-//Firebase: Need to provide options, when not being deployed to hosting via source. (app/no-options)
-initalizeFirebaseDb;
+import {
+  getDatabase,
+  get,
+  ref,
+  set,
+  onValue,
+  push,
+  update,
+} from 'firebase/database';
 
 const userAuthContext = createContext();
 
 export function UserAuthContextProvider({children}) {
-  // Set an initializing state whilst Firebase connects
+  //imported here  from Firebase.js so when authentication happen firebase is initalized as well, otherwise will be error:
+  //Firebase: Need to provide options, when not being deployed to hosting via source. (app/no-options)
+  initalizeFirebaseDb;
+
+  //Set an initializing state whilst Firebase connects
   const [initializing, setInitializing] = useState(true);
   const [user, setUser] = useState();
+  const [email, setEmail] = useState(null);
+  const [users, setUsers] = useState([]);
+  const [userToAdd, setUserToAdd] = useState(null);
+  const [selectedUser, setSelectedUser] = useState(null);
+  const [myData, setMyData] = useState(null);
 
+  //react - native firebase GoogleSignin packadge
   const _signInWithGoogle = async () => {
     try {
       GoogleSignin.configure({
@@ -34,20 +49,66 @@ export function UserAuthContextProvider({children}) {
       //   email: userInfo.user.email,              <<<=== from '@react-native-firebase/auth'
       //   photoUrl: userInfo.user.photo,           <<<=== https://rnfirebase.io/auth/usage
       // });
-      console.log('User logged in as ===>', userInfo.user.name.split(' ')[0]);
     } catch (error) {
       console.log('Sign in error: ===>', error);
     }
   };
 
-  // Handle user state changes on login saves user
+  async function checkUserInDb(user) {
+    console.log('checking user in db');
+    console.log(user.email);
+    console.log('====================================');
+    try {
+      const database = getDatabase();
+      //first check if the user registered before
+
+      const userObj = await findUser(email);
+
+      if (userObj) {
+        setMyData(userObj);
+      } else {
+        const newUserObj = {
+          name: user.displayName,
+          photo: user.photoURL,
+          email: user.email,
+          theme: "light"
+        };
+
+        set(ref(database, `users/${email}`), newUserObj);
+        setMyData(newUserObj);
+      }
+
+      // console.log(database);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  //finduser called in checkUserInDb
+  const findUser = async email => {
+    const database = getDatabase();
+    const mySnapshot = await get(ref(database, `users/${email}`));
+    return mySnapshot.val();
+  };
+
+  //handle user state changes on login saves user
   function onAuthStateChanged(user) {
-    setUser(user);
+    if (user != null) {
+      setEmail(user.email);
+      setUser({
+        email: user.email,
+        name: user.displayName,
+        photo: user.photoURL,
+      });
+      checkUserInDb(user);
+    } else {
+      setUser(null);
+    }
+    // console.log(user);
     if (initializing) setInitializing(false);
-    console.log('User is ===>', user);
-    console.log('User initializing ===>', initializing);
+    // console.log('User initializing ===>', initializing);
     if (initializing) return null;
-    console.log('User initializing ===>', initializing);
+    // console.log('User initializing ===>', initializing);
   }
 
   useEffect(() => {
