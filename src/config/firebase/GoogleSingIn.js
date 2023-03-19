72c077b4cb66e33a34b9d2c5f67b45bb7f03e7f2
Andras E <rohadtsajt@gmail.com>
2023-03-19 11:41:04 +0000

    x

--- src/config/firebase/GoogleSingIn.js
+++ src/config/firebase/GoogleSingIn.js
@@ -1,22 +1,5 @@
 import {GoogleSignin} from '@react-native-google-signin/google-signin';
 import auth from '@react-native-firebase/auth';
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDcrW4flxv6Pi0ejQZ45IQ9psjFDnU8k6U",
-  authDomain: "usapp-1d1ea.firebaseapp.com",
-  databaseURL: "https://usapp-1d1ea-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "usapp-1d1ea",
-  storageBucket: "usapp-1d1ea.appspot.com",
-  messagingSenderId: "826456309695",
-  appId: "1:826456309695:web:ded484e944c7448a406b0a"
-};
-
-// Initialize Firebase
-
 
 export const _signInWithGoogle = async () => {
   try {
@@ -33,12 +16,11 @@ export const _signInWithGoogle = async () => {
     const googleCredentials = auth.GoogleAuthProvider.credential(idToken);
     auth().signInWithCredential(googleCredentials);
     const user = {
-      name: userInfo.user.name.split(" ")[0],
+      name: userInfo.user.name.split(' ')[0],
       email: userInfo.user.email,
-      photoUrl: userInfo.user.photo
-    }
-    return (user);
-    
+      photoUrl: userInfo.user.photo,
+    };
+    return user;
   } catch (error) {
     console.log('=> Google Sign In Error:', error);
     return null;
