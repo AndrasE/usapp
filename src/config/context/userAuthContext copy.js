b9b88ec68b2e1abb72adda290805be6a1bb626a1
Andras E <rohadtsajt@gmail.com>
2023-03-23 22:42:45 +0000

    messing around test

--- src/config/context/userAuthContext copy.js
+++ src/config/context/userAuthContext copy.js
@@ -66,19 +66,19 @@ export function UserAuthContextProvider({children}) {
   // };
 
   //handle user state changes on login saves user, needed to debounce it as onAuthStateChange has multiple states,so useEffect would run 2-3x times making the checkUserDB() and create a loop, or if trying to set a state withing the onAuthState change, as it runs through multiple states will create undefined object first few times which cant be used in checkUserDb(), a bit meh situation but debouncing will prevent this to happen https://stackoverflow.com/questions/37673616/firebase-android-onauthstatechanged-called-twice //
-  var debounceTimeout;
-  const DebounceDueTime = 200; // 200ms
-  function onAuthStateChanged(user) {
-    if (debounceTimeout) clearTimeout(debounceTimeout);
-    debounceTimeout = setTimeout(() => {
-      debounceTimeout = null;
-      handleAuthStateChanged(user);
-    }, DebounceDueTime);
-  }
+  // var debounceTimeout;
+  // const DebounceDueTime = 200; // 200ms
+  // function onAuthStateChanged(user) {
+  //   if (debounceTimeout) clearTimeout(debounceTimeout);
+  //   debounceTimeout = setTimeout(() => {
+  //     debounceTimeout = null;
+  //     handleAuthStateChanged(user);
+  //   }, DebounceDueTime);
+  // }
   // console.log(user);
 
-  function handleAuthStateChanged(user) {
-    setUser(user);
+  // function handleAuthStateChanged(user) {
+  //   setUser(user);
     // if (initializing) setInitializing(false);
     // if (user !== null) {
     //   console.log('====> User is authenticated already as:', user.email);
@@ -86,13 +86,17 @@ export function UserAuthContextProvider({children}) {
     // } else {
     //   console.log('====> User not found or signed out:', user);
     // }
-  }
+  // }
 // console.log(".");
   useEffect(() => {
     const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    function onAuthStateChanged(user) {
+      setUser(user);
+      // if (initializing) setInitializing(false);
+    }
     return subscriber; // unsubscribe on unmount
   }, []);
-
+console.log();
   function logOut() {
     auth()
       .signOut()
