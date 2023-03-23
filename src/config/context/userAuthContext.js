09302275a401149b3d8642d4cfcc60ade5bc8773
Andras E <rohadtsajt@gmail.com>
2023-03-23 18:56:44 +0000

    tests not important

--- src/config/context/userAuthContext.js
+++ src/config/context/userAuthContext.js
@@ -23,19 +23,21 @@ export function UserAuthContextProvider({children}) {
   const [users, setUsers] = useState([]);
   const [userToAdd, setUserToAdd] = useState(null);
   const [selectedUser, setSelectedUser] = useState(null);
-  const [myData, setMyData] = useState(null);
+  const [myData, setMyData] = useState();
   const [user, setUser] = useState();
 
   const checkUserInDb = async user => {
-    console.log('=========> Checking user in database:', user.email);
-    const emailName = user.email.substring(0, user.email.indexOf('@'));
+    console.log('====> Checking user in database with email:', user.email);
+    console.log("======================================================================")
 
+    const emailName = user.email.substring(0, user.email.indexOf('@'));
     try {
       const database = getDatabase();
       //first check if the user registered before
       const userObj = await findUser(emailName);
       if (userObj) {
-        setMyData(userObj);
+        setMyData(userObj)
+        console.log('====> Found in Database with email:', user.email);
       } else {
         const newUserObj = {
           name: user.displayName,
@@ -46,12 +48,15 @@ export function UserAuthContextProvider({children}) {
         };
         set(ref(database, `users/${emailName}`), newUserObj);
         setMyData(newUserObj);
+        console.log('====> User created in Database with email:', user.email);
       }
-      console.log(myData);
     } catch (error) {
       console.error(error);
     }
+    console.log('====> User in Database:', myData);
+    console.log("======================================================================")
   };
+  //  console.log('====> User in Database:', myData);
 
   //finduser called in checkUserInDb
   const findUser = async emailName => {
@@ -75,13 +80,13 @@ export function UserAuthContextProvider({children}) {
     setUser(user);
     if (initializing) setInitializing(false);
     if (user !== null) {
-      console.log('=========> User is authenticated already:', user);
+      console.log('====> User is authenticated already as:', user.email);
       checkUserInDb(user);
     } else {
-      console.log('=========> User not found or signed out:', user);
+      console.log('====> User not found or signed out:', user);
     }
   }
-
+console.log(".");
   useEffect(() => {
     const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
     return subscriber; // unsubscribe on unmount
@@ -90,12 +95,12 @@ export function UserAuthContextProvider({children}) {
   function logOut() {
     auth()
       .signOut()
-      .then(() => console.log('=========> User signed out!'));
+      .then(() => console.log('====> User signed out!'));
   }
 
   return (
     <userAuthContext.Provider
-      value={{initializing, _signInWithGoogle, user, userDetails, logOut}}>
+      value={{initializing, _signInWithGoogle, user, myData, logOut}}>
       {children}
     </userAuthContext.Provider>
   );
