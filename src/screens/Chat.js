341a083b850b5594078902a0a6739ffd1d1e6a11
Andras E <rohadtsajt@gmail.com>
2024-01-16 19:13:27 +0000

    chat screen start

--- src/screens/Chat.js
+++ src/screens/Chat.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useState} from 'react';
+import {Text, View} from 'react-native';
 import {useUserTheme} from '../config/context/userThemeContext';
+import {useUserDb} from '../config/context/userDbContext';
 import LinearGradient from 'react-native-linear-gradient';
 import chatScreenStyles from '../styles/chatScreenStyles';
 
 export default function Chat() {
+  const [messages, setMessages] = useState([]);
   const {theme, textSize} = useUserTheme();
+  const {myData, selectedUser} = useUserDb();
   const styles = chatScreenStyles(theme, textSize);
-
 
   return (
     <>
@@ -18,7 +20,7 @@ export default function Chat() {
           locations={[0.0, 0.59]}
           colors={[theme.appbg1, theme.appbg2]}
           style={styles.linearGradientBackground}>
-        
+          <Text style={{color: 'red'}}>{selectedUser.friendsName}</Text>
         </LinearGradient>
       </View>
     </>
