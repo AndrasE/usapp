070bb072d67d913beb2197c5c66b1540879d0c23
Andras E <rohadtsajt@gmail.com>
2024-01-20 11:53:40 +0000

    x

---
+++ src/config/firebase/GoogleSingIn.js
@@ -0,0 +1,21 @@
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
+import auth from '@react-native-firebase/auth';
+import { WEBCLIENTID } from '@env';
+
+export const _signInWithGoogle = async () => {
+  try {
+    GoogleSignin.configure({
+      offlineAccess: false,
+      scopes: ['profile', 'email'],
+      webClientId: WEBCLIENTID,
+    });
+    await GoogleSignin.hasPlayServices();
+    const {idToken} = await GoogleSignin.signIn();
+    const googleCredentials = auth.GoogleAuthProvider.credential(idToken);
+    auth().signInWithCredential(googleCredentials);
+    return;
+  } catch (error) {
+    console.log('=> Google Sign In Error:', error);
+    return null;
+  }
+};
