7ed10479b3a0a6fd29b4eaac6550a9b552f376d6
Andras E <rohadtsajt@gmail.com>
2024-01-30 19:30:11 +0000

    x

--- App.js
+++ App.js
@@ -1,6 +1,4 @@
 import React, {useState, useEffect} from 'react';
-import {Alert} from 'react-native';
-
 import {NavigationContainer} from '@react-navigation/native';
 import {UserAuthContextProvider} from './src/config/context/userAuthContext';
 import {UserDbContextProvider} from './src/config/context/userDbContext';
@@ -8,18 +6,15 @@ import {UserThemeContextProvider} from './src/config/context/userThemeContext';
 import {useUserAuth} from './src/config/context/userAuthContext';
 import {SplashScreen, SignInScreen} from './src/navigations/ScreensImport';
 import DrawerNavigator from './src/navigations/DrawerNavigator';
-import {messaging, onMessage, onNotification, onNotificationOpened} from '@react-native-firebase/messaging';
-import { firebase } from '@react-native-firebase/messaging';
-
+import messaging from '@react-native-firebase/messaging';
+import {PermissionsAndroid} from 'react-native';
+PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
 
 function RootNavigator() {
-
-
   // await splash screen to finish the animation and firebase to get connected and establish //
   // if the user is authenticated and call homestack to conditinally render //
   const [splash, setSplash] = useState(true);
   const {user} = useUserAuth();
-
 
   setTimeout(() => {
     setSplash(false);
@@ -53,52 +48,44 @@ function RootNavigator() {
   }
 }
 
-// Necessary to wrap the Home/Login stacks in order to have access to the Context //
-// <userAuthContext.Provider value={{...}}> {children} </userAuthContext.Provider> //
-
 export default function App() {
-  const [token, setToken] = useState("")
+  const [token, setToken] = useState('');
 
-  useEffect(() => {
-    firebase.messaging().getToken()
-  .then(fcmToken => {
-    if (fcmToken) {
-      console.log(fcmToken)
-      setToken(fcmToken)
-      // user has a device token
-    } else {
-      // user doesn't have a device token yet
-    } 
+  // messaging().onMessage(async remoteMessage => {
+  //   console.log('Message handled in the backgrossaund!', remoteMessage);
+  // });
+  messaging().setBackgroundMessageHandler(async remoteMessage => {
+    console.log('Message handled in the background!', remoteMessage);
   });
+
+  function requestAndroidPermission() {
+    PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
+  }
+
+  async function requestUserPermission() {
+    const authorizationStatus = await messaging().requestPermission();
+  
+    if (authorizationStatus) {
+      console.log('Permission status:', authorizationStatus);
+    }
+  }
+  
+  useEffect(() => {
+    const setUpCloudMessaging = async () => {
+      requestUserPermission();
+
+      const token = await messaging().getToken();
+      console.log('Token is ' + token);
+    };
+    setUpCloudMessaging();
   }, []);
 
-
-//     //check whether the initalapp is available
-    // messaging()
-    //   .getInitalNotification()
-    //   .then(remoteMessage => {
-    //     if (remoteMessage) {
-    //       console.log(
-    //         'Notification caused the app to opeb from quit state',
-    //         remoteMessage.notification,
-    //       );
-    //     }
-    //   });
-
-//     //type data payload of the screen to open
-
-//     messaging().onNotificationOpenedApp(remoteMessage => {
-//       console.log(
-//         'Notification caused the app to opeb from quit state',
-//         remoteMessage.notification,
-//       );
-//     });
-//   }, []);
-
+  // Necessary to wrap the Home/Login stacks in order to have access to the Context //
+  // <userAuthContext.Provider value={{...}}> {children} </userAuthContext.Provider> //
   return (
     <UserAuthContextProvider>
       <UserDbContextProvider fcmToken={token}>
-        <UserThemeContextProvider >
+        <UserThemeContextProvider>
           <RootNavigator />
         </UserThemeContextProvider>
       </UserDbContextProvider>
