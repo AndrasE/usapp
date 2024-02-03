251506f307360d55be91dacfa0d845632c3282e4
Andras E <rohadtsajt@gmail.com>
2024-02-03 19:15:44 +0000

    Update asd.js

--- src/screens/asd.js
+++ src/screens/asd.js
@@ -1,10 +1,48 @@
 import React from 'react';
-import {Text, ImageBackground, Button} from 'react-native';
-import Animated, {FadeIn} from 'react-native-reanimated';
-import Lottie from 'lottie-react-native';
+import {ImageBackground, Button} from 'react-native';
 
 export default function HomeScreen() {
- 
+
+  const sendMessageToDevice = async () => {
+    // Construct the request body
+    const requestBody = {
+      message: {
+        token: 'dKtj2AXzQkeDHsxHi3jtZF:APA91bGdSInb2chtr6dc92uHStRV83dpCLGq6UrefbIZSgtW8cxwvjuBGZ5GGLdFy-T9kBKXYAa3vumBWffV3_QutPy6C7Ir-NhL-A2E7XHYnwWDVUDjFU9FzadsFUD66DEpk16ASC15',
+        notification: {
+          title: 'Your notification title',
+          body: 'Your notification body',
+        },
+        data: {
+          key1: 'value1',
+          key2: 'value2',
+        },
+      },
+    };
+  
+    // Set the request headers
+    const headers = {
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer ya29.a0AfB_byDEryIpLK9eeHlgJHXQ0Y7VxuVHla_SjUyRXuYioD-VZg6Lo1EdXRmKcuHd8FnbTpN0WRFxyb6SXjB0ogF9nnYfLoaRtTEcYmeCjG9QuC7gHB-xSJdYocByW-SPm673YpGnIquUzWAnVMAOr5zQCpfybAifcaQKaCgYKAVkSARASFQHGX2MiIis28F_4KMVd_IwQ0LIgtg0171'
+    };
+  
+    // Construct the API request URL
+    const url = `https://fcm.googleapis.com/v1/projects/usapp-1d1ea/messages:send`;
+  
+    try {
+      // Make the API request using fetch
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(requestBody),
+      });
+  
+      const data = await response.json();
+      console.log('Message sent successfully:', data);
+    } catch (error) {
+      console.log('Error sending message:', error);
+    }
+  };
+
   return (
     <ImageBackground
       source={require('../assets/backgroundImage.jpg')}
@@ -14,13 +52,8 @@ export default function HomeScreen() {
         alignItems: 'center',
         padding: 30,
       }}>
-      <Lottie
-        style={{paddingTop: 70, height: 170}}
-        source={require('../assets/lottieAnimations/search.json')}
-        autoPlay
-        loop={false}
-        speed={0.7}
-      />
+      <Button onPress={sendMessageToDevice} title='asdasd'/>
+  
     </ImageBackground>
   );
 }
