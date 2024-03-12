00d91c4325d435afea44678daccba46eb81b0037
Andras E <rohadtsajt@gmail.com>
2024-03-12 20:05:37 +0000

    Update AboutMore.js

--- src/screens/AboutMore.js
+++ src/screens/AboutMore.js
@@ -196,11 +196,103 @@ export default function AboutMore() {
               </Text>
               <Text style={styles.subHeader}>Packadges</Text>
               <Text style={styles.paragraph}>
-              Here are some rn packages I needed for all this to come together: async-storage, react-native animated, modal, linear-gradient, moment, switch-selector, lottie, axios, google-signin, vector-icons, firebase/auth, firebase/app, onesignal and of course gifted-chat. 
+                Here are some rn packages I needed for all this to come
+                together: async-storage, react-native animated, modal,
+                linear-gradient, moment, switch-selector, lottie, axios,
+                google-signin, vector-icons, firebase/auth, firebase/app,
+                onesignal and of course gifted-chat.
               </Text>
               <Text style={styles.subHeader}>Authentication</Text>
               <Text style={styles.paragraph}>
-              As soon as you click the application icon there a splash screen welcomes you. While the animation of this screen takes place there are many things happening in the background. Firstly checking if the user has previously logged in, if so it will be persistent using onAuthStateChanged from RN-firebase. If this is the first time logging in or being logged out previously we will be redirected to the login screen. Using async-storage the users settings (theme and text-size) saved in the device this will be passed on to the application via userThemeContext.js, it is not set before it will load with a default option. If the user logging in first time using _signInWithGoogle from react-native-google-signin we capture a few details from the user (name, email-address, profile pic), Using these we passed this on via userAuthContext.js and we create an entry in the database with the username set to the email address before the @ symbol. This will be passed onto the app via userDbContext.js. If the user has previously logged in will be present in the database and we can capture a snapshot of the belonging details and serve it to the app. 
+                As soon as you click the application icon there a splash screen
+                welcomes you. While the animation of this screen takes place
+                there are many things happening in the background. Firstly
+                checking if the user has previously logged in, if so it will be
+                persistent using onAuthStateChanged from RN-firebase. If this is
+                the first time logging in or being logged out previously we will
+                be redirected to the login screen. Using async-storage the users
+                settings (theme and text-size) saved in the device this will be
+                passed on to the application via userThemeContext.js, it is not
+                set before it will load with a default option. If the user
+                logging in first time using _signInWithGoogle from
+                react-native-google-signin we capture a few details from the
+                user (name, email-address, profile pic), Using these we passed
+                this on via userAuthContext.js and we create an entry in the
+                database with the username set to the email address before the @
+                symbol. This will be passed onto the app via userDbContext.js.
+                If the user has previously logged in will be present in the
+                database and we can capture a snapshot of the belonging details
+                and serve it to the app. The user also will be subscribed to
+                Onesignal services and logged in their database so later on push
+                notification can be sent to them.
+              </Text>
+              <Text style={styles.subHeader}>Gifted-chat</Text>
+              <Text style={styles.paragraph}>
+                Gifted-chat takes a lot of work from our end, however you could
+                use a flatlist to get similar results. Now that we have a
+                database, every change (friends added, messages sent) will be
+                logged in our database and we can access all this with
+                userDbContext.js. If the person doesn't have a friend then there
+                will be a small animation and instruction showing what to do.
+                When you search for a friend it checks in the database if it
+                exists, if does it will create a new entry in the database as in
+                the chatrooms section containing the details of the participants
+                and create a friend for both users with each other's details and
+                the with the chatroomId which we can use to access later on.
+                When we click on the chat we are trying to access by passing
+                this id we can fetch the messages previously sent/received. When
+                a new message is sent the given chat room will be updated with
+                the new message and onValue from firebase/database will be
+                triggered and a new snapshot will be created of the chatroom and
+                displayed to the user.
+              </Text>
+              <Text style={styles.subHeader}>Push notifications</Text>
+              <Text style={styles.paragraph}>
+                Oh boi, I learnt quite a bit from this. I never suspected how
+                this works, I thought it would be something simple, but now
+                looking back it does make a lot of sense due to security. I
+                tried and succeeded to send push notifications to devices with
+                cloud-messaging(FCM) and notifee, but eventually I settled with
+                OneSignal. It's slightly more complicated in this app case than
+                for example a standalone app is that we need to be able to send
+                notification to other users from our phone without having admin
+                privileges and also securely. What happens is when you logging
+                in, the OneSignal database will create a user with the username
+                we used in our database, which will be needed to know who is the
+                sender and the receiver of the the pish notification. As we are
+                already speaking with a friend and have its username in our
+                database thats also will be a id of the user in onesignals
+                database so we will know who should be the recipient of the push
+                notification. What I found interesting is that these push
+                notifications are not something I was thinking of before and how
+                it can be used for marketing and engaging purposes. Looking at
+                both google and onesignals dashboard you could create whole
+                campaigns and measure of its success rate, who clicked, who
+                swiped it away and so forth. Now I can see how this is a very important feature of an app. You could send reminders and item`s price has dropped or an abandoned item in the basket or for example try to get away by not doing your lesson on duolingo you definitely will be reminded. 
+              </Text>
+              <Text style={styles.subHeader}>Shortcomings</Text>
+              <Text style={styles.paragraph}>
+                Oh boi, I learnt quite a bit from this. I never suspected how
+                this works, I thought it would be something simple, but now
+                looking back it does make a lot of sense due to security. I
+                tried and succeeded to send push notifications to devices with
+                cloud-messaging(FCM) and notifee, but eventually I settled with
+                OneSignal. It's slightly more complicated in this app case than
+                for example a standalone app is that we need to be able to send
+                notification to other users from our phone without having admin
+                privileges and also securely. What happens is when you logging
+                in, the OneSignal database will create a user with the username
+                we used in our database, which will be needed to know who is the
+                sender and the receiver of the the pish notification. As we are
+                already speaking with a friend and have its username in our
+                database thats also will be a id of the user in onesignals
+                database so we will know who should be the recipient of the push
+                notification. What I found interesting is that these push
+                notifications are not something I was thinking of before and how
+                it can be used for marketing and engaging purposes. Looking at
+                both google and onesignals dashboard you could create whole
+                campaigns and measure of its success rate, who clicked, who
+                swiped it away and so forth. Now I can see how this is a very important feature of an app. You could send reminders and item`s price has dropped or an abandoned item in the basket or for example try to get away by not doing your lesson on duolingo you definitely will be reminded. 
               </Text>
             </Animated.View>
           </View>
